25fedd4df3d3316e6e4f5156396df8a2d8374c79
FilipinoAkoYT <anjomartin.meras@depedparanaquecity.com>
2022-02-02 16:52:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,9 @@ var BotActivityType = "undefined"
 var Warn = "undefined"
 var WordsList = ["fuck", "shit", "bitch", "asshole", "motherfucker"]
 var Warntype = "undefined"
-var DMWarn = "undefined"
+// Now Using Normal Warn
+// var DMWarn = "undefined"
+var UptimePort = 3000
 
 exports.wordblocker = function (input) {
   BotToken = input.bot_token
@@ -41,45 +43,51 @@ exports.wordblocker = function (input) {
   Warn = input.word_warn
   Warntype = input.word_warn_type
   console.log('Warn & WarnType Successfully Setted')
+	
+	
+  UptimePort = input.uptime_port
+  console.log(`Uptimer Port Setted To ${UptimePort}`)
 }
 
-exports.token = function (token) {
-  BotToken = token
-  console.log(`Token ${BotToken} Is Ready`)
-}
+// This Codes Below Been Disabled Now
+// exports.token = function (token) {
+//   BotToken = token
+//   console.log(`Token ${BotToken} Is Ready`)
+//}
 
-exports.activity = function (activity, activitytype) {
-       if(activitytype == 'PLAYING') {
-    BotActivityType = activitytype  
-} else if(activitytype == 'LISTENING') {
-    BotActivityType = activitytype
-} else if(activitytype == 'STREAMING') {
-    BotActivityType = activitytype
-} else if(activitytype == 'WATCHING') {
-    BotActivityType = activitytype
-} else if(activitytype == 'undefined') {
-    throw new Error('Activity Type Undefined')
-} else {
-    throw new Error('Invalid Activity Type')
-}
-  BotActivity = activity
-}
+// exports.activity = function (activity, activitytype) {
+//        if(activitytype == 'PLAYING') {
+//     BotActivityType = activitytype  
+// } else if(activitytype == 'LISTENING') {
+//     BotActivityType = activitytype
+// } else if(activitytype == 'STREAMING') {
+//     BotActivityType = activitytype
+// } else if(activitytype == 'WATCHING') {
+//     BotActivityType = activitytype
+// } else if(activitytype == 'undefined') {
+//     throw new Error('Activity Type Undefined')
+// } else {
+//     throw new Error('Invalid Activity Type')
+// }
+//   BotActivity = activity
+// }
 
-exports.words = function (words) {
-  WordsList = words
-  console.log('WordList Loaded Succesfully')
-}
+// exports.words = function (words) {
+//   WordsList = words
+//   console.log('WordList Loaded Succesfully')
+// }
 
-exports.warn = function (type, warn) {
-    Warntype = type
-    Warn = warn
-    console.log('Warn & WarnType Successfully Setted')
-}
+// exports.warn = function (type, warn) {
+//     Warntype = type
+//     Warn = warn
+//     console.log('Warn & WarnType Successfully Setted')
+// }
 
-exports.dmwarn = function (message) {
-    DMWarn = message
-    console.log(`DMWarn Message Successfully Setted`)
-}
+// exports.dmwarn = function (message) {
+//     DMWarn = message
+//     console.log(`DMWarn Message Successfully Setted`)
+// }
+// This Been Disabled Cuz Of The Easier Setup
 
 exports.start = function () {
      if(BotToken == 'undefined') {
@@ -103,7 +111,7 @@ exports.start = function () {
 const containsSmallWords =  WordsList.some(Smallword => message.content.toLowerCase().includes(Smallword))
 const containsBigWords =  WordsList.some(Bigword => message.content.toUpperCase().includes(Bigword))		
 
-if (Warntype == '1') {
+if (Warntype == 1) {
    if(containsSmallWords){ 
   message.channel.send(Warn)
 	message.delete()
@@ -113,7 +121,7 @@ if (Warntype == '1') {
 	message.delete()
 	console.log(`Successfully Warned ${message.author} And Deleted "${message.content}"`)
    }
-} else if (Warntype == '2') {
+} else if (Warntype == 2) {
    if(containsSmallWords){ 
   message.reply(Warn)
 	console.log(`Successfully Replied To ${message.author}, Word Used:"${message.content}"`)
@@ -121,7 +129,7 @@ if (Warntype == '1') {
   message.reply(Warn)
   console.log(`Successfully Replied To ${message.author}, Word Used:"${message.content}"`)
    }
-} else if (Warntype == '3') {
+} else if (Warntype == 3) {
    if(containsSmallWords){ 
 	message.delete()
   console.log(`Successfully Deleted "${message.content}", Author:"${message.author}"`)
@@ -129,35 +137,35 @@ if (Warntype == '1') {
 	message.delete()
   console.log(`Successfully Deleted "${message.content}", Author:"${message.author}"`)
    }
-} else if (Warntype == '4') {
+} else if (Warntype == 4) {
    if(containsSmallWords){ 
-	message.author.send(DMWarn)
+	message.author.send(Warn)
   console.log(`Successfully DM To "${message.author}", Word:"${message.content}"`)
    } else if (containsBigWords){
-  message.author.send(DMWarn)
+  message.author.send(Warn)
 console.log(`Successfully DM To "${message.author}", Word:"${message.content}"`)
    }
-} else if (Warntype == '5') {
+} else if (Warntype == 5) {
    if(containsSmallWords){ 
-	message.author.send(DMWarn)
+	message.author.send(Warn)
 	message.delete()
   console.log(`Successfully DMed To "${message.author}", And Deleted Word:"${message.content}"`)
    } else if (containsBigWords){
-  message.author.send(DMWarn)
+  message.author.send(Warn)
   message.delete()
   console.log(`Successfully DMed To "${message.author}", And Deleted Word:"${message.content}"`)
    }
 } else if (Warntype == 'undefined') {
    throw new Error('WarnType Undefined')
-} else if (Warntype != '1') {
+} else if (Warntype != 1) {
    throw new Error('Invalid Warn Type')
-} else if (Warntype != '2') {
+} else if (Warntype != 2) {
    throw new Error('Invalid Warn Type')
-} else if (Warntype != '3') {
+} else if (Warntype != 3) {
    throw new Error('Invalid Warn Type')
-} else if (Warntype != '4') {
+} else if (Warntype != 4) {
    throw new Error('Invalid Warn Type')
-} else if (Warntype != '5') {
+} else if (Warntype != 5) {
    throw new Error('Invalid Warn Type')
 }
 	})
@@ -167,5 +175,5 @@ exports.uptime = function () {
     server.all('/', (req, res)=>{
     res.send(`Uptimer Now Ready! Bot Username ${client.user.tag}`)
 })
-    server.listen(3000, ()=>{console.log("Uptime Server Now Ready")});
+    server.listen(UptimePort, ()=>{console.log(`Uptime Server Now Ready.. Listening To http://localhost:${UptimePort}`)});
 }
